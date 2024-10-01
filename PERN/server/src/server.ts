d0926f57594bd17f0a321f774f085ce041111c04
Zyndrr <erin.d.mckanna@gmail.com>
2024-09-30 20:57:39 -0600

    Set up Erin-Sign-Up branch

--- PERN/server/src/server.ts
+++ PERN/server/src/server.ts
@@ -1,18 +1,18 @@
-import express from 'express';
-import sequelize from './config/connection.js';
-import routes from './routes/index.js';
+import express from "express";
+// import sequelize from './config/connection.js';
+import routes from "./routes/index.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
+app.use(express.static("../client/dist"));
 
 app.use(express.json());
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-  });
+// sequelize.sync({ force: false }).then(() => {
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
+// });
