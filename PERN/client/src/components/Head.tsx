6c7945172dbc9af52018e00957f640903665974a
Zyndrr <erin.d.mckanna@gmail.com>
2024-10-09 19:47:39 -0600

    add login button

--- PERN/client/src/components/Head.tsx
+++ PERN/client/src/components/Head.tsx
@@ -1,13 +1,28 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import { useEffect, useState } from "react";
+import axios from "axios";
 //import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Head() {
+  const [userDetails, setUserDetails] = useState({ username: "" });
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const jwt = localStorage.getItem("jwt");
+        const response = await axios.post(`/api/user/verify`, { token: jwt });
+        setUserDetails(response.data);
+      } catch {
+        return;
+      }
+    };
+    getUserData();
+  });
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -24,7 +39,11 @@ export default function Head() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Buy Curious
           </Typography>
-          <Button color="inherit">Login</Button>
+          {!userDetails.username && (
+            <Button href="/login" color="inherit">
+              Login
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
