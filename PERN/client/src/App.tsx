add0dc081a1ba65e9956f03444e0a0a03d7af1a2
Zyndrr <erin.d.mckanna@gmail.com>
2024-10-09 20:26:28 -0600

    remove theme login

--- PERN/client/src/App.tsx
+++ PERN/client/src/App.tsx
@@ -2,34 +2,34 @@ import React, { useMemo } from "react";
 
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { useState } from "react";
 import { Button, PaletteMode } from "@mui/material";
 import Head from "./components/Head";
 
-
-
-
 export default function App() {
-  const [theme, setTheme] = useState('dark' as PaletteMode); 
-  const themeData = useMemo(() =>  createTheme({
-    palette: {
-      mode: theme,
-    },
-  }),[theme]);
-
-  
+  const [theme, setTheme] = useState("dark" as PaletteMode);
+  const themeData = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: theme,
+        },
+      }),
+    [theme]
+  );
 
   return (
     <>
       <ThemeProvider theme={themeData}>
         <CssBaseline />
-        <Button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>Toggle Theme</Button>
-        <Head/>
+        <Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+          {theme === `dark` ? `Light` : `Dark`} Mode
+        </Button>
+        <Head />
         <Outlet />
       </ThemeProvider>
-
     </>
   );
 }
