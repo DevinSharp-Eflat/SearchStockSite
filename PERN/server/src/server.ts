215c439a49edf4396ddfecbe422fd1952588faa3
Devin Sharp <sharpdevin467@gmail.com>
2024-10-09 22:13:06 -0700

    finished? pt2

--- PERN/server/src/server.ts
+++ PERN/server/src/server.ts
@@ -15,7 +15,7 @@ app.use(cors());
 const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
-app.use(express.static("../../client/dist"));
+app.use(express.static("../client/dist"));
 
 app.use(express.json());
 app.use(routes);
